92e1ad4e1688bd16ea972b159dff7eeb61395b25
Teja Kunderu <tejakashyap@gmail.com>
2017-11-17 02:41:27 -0700

    added logout and signup in routes

--- app/server/modules/account-manager.js
+++ app/server/modules/account-manager.js
@@ -1,4 +1,4 @@
-// start coding here for backend functionalities and processing
+// Backend functionality
 
 const {Pool, Client} = require('pg');
 const client = new Client({
@@ -6,48 +6,41 @@ const client = new Client({
     host: 'awdbinstance.crgkoefokbdm.us-east-1.rds.amazonaws.com',
     database: 'awDb',
     password: 'Adaptiveweb123',
-    port: 5432,
+    port: 5432
 });
 client.connect();
 
 const bcrypt = require('bcrypt');
 
-// create multiple files for different fucntionalities
-
-// This file is for Maintaining account
-
 exports.manualLogin = function (user, pass, callback) {
     client.query("SELECT * FROM user_details where username = $1", [user], (err, result) => {
-        if (result.rows.length == 1) {
+        if (result.rows.length === 1) {
             var password = result.rows[1];
             bcrypt.compare(pass, password, function(err, res) {
-                if(res) {
+                if (res) {
                     callback(true, 'Logged in');
                 } else {
                     callback(true, 'Invalid Password');
                 }
             });
-        }
-        else {
+        } else {
             callback(false, "Invalid user");
         }
-    });
-}
+    })
+};
 
 
 exports.updateActivity = function (user, activity, callback) {
-}
+};
 
-exports.displayQuestion = function(questionId, callback){
-
-    client.query("SELECT * from question_and_answer where question_id =$1", [questionId], (err,result) =>{
-        if (result.rows.length == 1) {
+exports.displayQuestion = function (questionId, callback) {
+    client.query("SELECT * from question_and_answer where question_id =$1", [questionId], (err,result) => {
+        if (result.rows.length === 1) {
                 callback(true, //JSON.stringify(result.rows[0]));
                                      result.rows[0]);
-        }
-        else {
+        } else {
             callback(false, "Invalid Question Id");
         }
-    });
+    })
 
-}
+};
--- app/server/routes.js
+++ app/server/routes.js
@@ -1,91 +1,91 @@
 // Controller file
+
 var AM = require('./modules/account-manager');
 var RE = require('./modules/recommendor');
 
-
 module.exports = function(app) {
 
-// Root login page redirects to home page if user already logged in else to the login page //
 	app.get('/', function(req, res){
-        if (req.session.loggedin == null || req.session.loggedin == false){
-            res.render("form-login",{error:""})
-        }
-        else {
-           res.redirect("/home");
+        if (req.session.loggedin === undefined || req.session.loggedin === false){
+            res.render('form-login', {error:''})
+        } else {
+           res.redirect('/home');
         }
 	});
 
     app.post('/login', function(req, res){
-        if (req.session.loggedin == null || req.session.loggedin == false){
+        if (req.session.loggedin === undefined || req.session.loggedin === false){
             AM.manualLogin(req.body.username, req.body.password, function(status, o){
                 if(status) {
                     req.session.loggedin = true;
-                    res.redirect("/home");
+                    res.redirect('/home');
                 }
                 else {
                     res.render('form-login', {error : o});
                 }
             });
-        }
-        else {
-            if(req.session.loggedin == true) {
-                res.redirect("/home");
-            }
-            else {
-                res.render("form-login", {error : "Please login again"})
+        } else {
+            if(req.session.loggedin === true) {
+                res.redirect('/home');
+            } else {
+                res.render('form-login', {error : 'Please login again'})
             }
         }
     });
 
-// Logs in the user on based on the username and password entered or already logged in users are redirected to home page //
-
 	app.get('/home', function(req, res){
-	    /*
-		AM.manualLogin(req.body['username'], req.body['password'], function(e, o){
-        });*/
-        question_id=Math.floor(Math.random() * 101);
-        AM.displayQuestion(question_id, function(status, result){
-            if(status){
-                res.render('home.ejs', {
-                    question: result.question,
-                    num_choices: result.num_choices,
-                    choice1 : result.choice_a,
-                    choice2 : result.choice_b,
-                    choice3 : result.choice_c,
-                    choice4 : result.choice_d,
-                    choice5 : result.choice_e,
-                    correct : result.answer
+        if (req.session.loggedin === undefined || req.session.loggedin === false){
+            res.redirect('/');
+        } else {
+            question_id=Math.floor(Math.random() * 101);
+            AM.displayQuestion(question_id, function (status, result) {
+                var question = result.question.trim();
+                if (question[0] === '"') {
+                    question = question.substring(1, question.length - 1);
+                }
+                if (status) {
+                    res.render('home.ejs', {
+                        question: question,
+                        num_choices: result.num_choices,
+                        choice1 : result.choice_a,
+                        choice2 : result.choice_b,
+                        choice3 : result.choice_c,
+                        choice4 : result.choice_d,
+                        choice5 : result.choice_e,
+                        correct : result.answer
 
-                });
-                console.log(result.answer);
-            }
-        });
+                    });
+                    console.log(result.answer);
+                }
+            });
+        }
 	});
 
     app.get('/userProfile', function(req, res){
-
-        res.render("user-profile")
-        /*
-        AM.manualLogin(req.body['username'], req.body['password'], function(e, o){
-        });*/
+        if (req.session.loggedin === undefined || req.session.loggedin === false){
+            res.redirect('/');
+        } else {
+            res.render('user-profile');
+        }
     });
 
     app.post('/quiz', function(req, res){
-
-        res.render("home")
-        /*
-        AM.manualLogin(req.body['username'], req.body['password'], function(e, o){
-        });*/
-
-
+        res.render('home');
     });
 
     app.get('/signup', function(req, res){
+        if (req.session.loggedin === undefined || req.session.loggedin === false){
+            res.render('form-register', {error : ''});
+        } else {
+            res.redirect('/home');
+        }
+    });
 
-        res.render("form-register", {error : ""});
-        /*
-        AM.manualLogin(req.body['username'], req.body['password'], function(e, o){
-        });*/
+    app.get('/logout', function(req, res){
+        if (req.session.loggedin === true) {
+            req.session.loggedin = false;
+        }
+        res.redirect('/');
     });
 
 };
